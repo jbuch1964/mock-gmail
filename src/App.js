ae66cb6486e8fbc59aa41222ef224f18c4a91123
jbuchanon <jbuch1964@gmail.com>
2021-10-06 17:52:14 -0400

    Finally

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React {useState} from 'react'
 import logo from './logo.svg';
-import './App.css';
+import './index.css';            //'./App.css';
 
+          // From below 1st <p>, Edit <code>src/App.js</code> and save to reload.
+//              <button className="btn btn-warning">Search</button>
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p> 
+          </p>
+          <form action="127.0.0.1:3000" method="GET">
+              <div className="form-example">
+                <label for="name">Enter your name: </label>
+                <input type="text" name="name" id="name" required>
+              </div>
+          </form>          
       </header>
     </div>
   );
